fa64d1f74251b7446ee78178b9e43e9f08206280
Toufiq-it <ittoufikit@gmail.com>
2025-09-09 02:30:50 +0600

    navber footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Button } from "./components/ui/button"
+import { Outlet } from "react-router"
+import CommonLayout from "./components/layout/CommonLayout"
 
 function App() {
 
   return (
-    <div>
-      <h1>Welcome To our digital wallet app</h1>
-      <Button>Click Me</Button>
-    </div>
+    <CommonLayout>
+      <Outlet />
+    </CommonLayout>
   )
 }
 
