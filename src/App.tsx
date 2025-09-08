466cd992ab38c213995a5c03b1aee47c965614c3
Toufiq-it <ittoufikit@gmail.com>
2025-09-08 23:36:40 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome To our digital wallet app</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
